db4937e39f45e1f4ff3dc6b83ce54b62fd5dc1e3
Cristhian Vargas <cristhian.vargas@internationalsos.com>
2024-06-19 23:17:15 -0500

    avance

--- src/AppContainer.vue
+++ src/AppContainer.vue
@@ -1,27 +1,25 @@
 <template>
   <v-app id="inspire">
-    <v-app-bar class="px-3" density="compact" flat>
-      <v-avatar class="hidden-md-and-up" color="grey-darken-1" size="32"></v-avatar>
+    <v-app-bar class="px-3" density="compact" >
+      <v-img src="@/assets/cinerama.svg" alt="Logo" max-width="130"></v-img>
       <v-spacer></v-spacer>
       <v-tabs color="grey-darken-2" centered>
         <v-tab v-for="link in links" :key="link" :text="link"></v-tab>
       </v-tabs>
       <v-spacer></v-spacer>
-      <v-avatar class="hidden-sm-and-down" color="grey-darken-1" size="32"></v-avatar>
     </v-app-bar>
     <v-main class="bg-grey-darken-1">
-        <v-row class="mt-1">
-          <v-col cols="12" offset-xl="2" offset-lg="1" xl="8" lg="10" >            
-            <v-sheet min-height="70vh" rounded="lg">
-              <!--  -->
-            </v-sheet>
-          </v-col>
-        </v-row>
+      <v-row class="mt-1">
+        <v-col cols="12" offset-xl="2" offset-lg="1" xl="8" lg="10">
+          <v-sheet min-height="70vh" rounded="lg">
+            <!--  -->
+          </v-sheet>
+        </v-col>
+      </v-row>
 
     </v-main>
   </v-app>
 </template>
-
 <script setup>
 const links = [
   'Peliculas',
@@ -30,7 +28,6 @@ const links = [
   'Dulceria',
 ]
 </script>
-
 <script>
 export default {
   data: () => ({
