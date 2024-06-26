221c546dfd8c2f7b88cb28c34be04b664c23f9c5
Cristhian vargas <cristhian199228@gmail.com>
2024-06-26 16:01:30 -0500

    feat(skeleton_complete): skeleton_complete
    se termino la estructura de la pagina y se introdujo el boton buscar

--- src/views/CarteleraView.vue
+++ src/views/CarteleraView.vue
@@ -1,15 +1,17 @@
 <template>
-  Elije tu cine
   <v-row class="ma-5">
     <v-col class="d-inline-flex">
       <v-select class="mx-2" label="Ciudad"
-        :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"></v-select>
+        :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']" hide-details></v-select>
       <v-select class="mx-2" label="Cine"
-        :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"></v-select>
+        :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']" hide-details></v-select>
       <v-select class="mx-2" label="Pelicula"
-        :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"></v-select>
-        <v-select class="mx-2" label="Fecha"
-        :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"></v-select>
+        :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']" hide-details></v-select>
+      <v-select class="mx-2" label="Fecha"
+        :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']" hide-details ></v-select>
+      <v-btn color="red-accent-4" class="my-auto">
+        Button
+      </v-btn>
     </v-col>
   </v-row>
 
